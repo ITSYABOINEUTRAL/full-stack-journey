f34bcf48731a22719bec7f26ab79d25f743146a4
ITSYABOINEUTRAL <okenwa.nwagu@mcis.sch.ng>
2026-08-21 16:49:52 +0100

    feat: added code for the last session of  week 3

---
+++ week-3/js/conditional-statement.js
@@ -0,0 +1,90 @@
+// One Conditional Statement
+
+// const age = 16;
+
+// if (age >= 20) {
+//     console.log("You are an Adult");
+// } else {
+//     console.log("You are a Child")
+// }
+
+// Multi-Conditional Statement
+
+// const score = 75;
+
+// if(score >= 70) {
+//     console.log("(A)");
+// } else if(score >= 60) {
+//     console.log("(B)");
+// } else if(score >= 50) {
+//     console.log("(C)");
+// } else if(score >= 40) {
+//     console.log("(D)");
+// } else {
+//     console.log("(F)");
+// }
+
+
+// Practice
+// const studentName = "Aisha";
+// const score = 76;
+
+// let grade;
+
+// if(score >= 70) {
+//     grade = "(A)";
+// } else if(score >= 60) {
+//     grade = "(B)";
+// } else if(score >= 50) {
+//     grade = "(C)";
+// } else if(score >= 40) {
+//     grade = "(D)";
+// } else {
+//     grade = "(F)";
+// }
+
+// console.log(`${studentName}'s grade is ${grade} `)
+
+
+// Class Work
+
+// const number = -5;
+// if(number > 0) {
+//     console.log("Positive Number");
+// } else if(number < 0) {
+//     console.log("Negative Number");
+// }
+
+// Switch
+
+// const day = "Tuesday";
+
+// switch(day) {
+//     case "Monday" :
+//         console.log("Start of the Week");
+//         break
+//     case "Friday" :
+//         console.log("Start of the Weekend"); 
+//         break
+//     case "Sunday" :
+//         console.log("End of the Weekend");
+//         break
+//     default: console.log("Regular Day");
+// }
+
+
+
+
+// Assignment
+
+const admin = "Abdul";
+const password = "1234";
+
+// Check if both values are true, if they are true print out "Login Successful!", if not "Admin or Password is incorrect, try again."
+
+// Solution
+if((admin === "Abdul") && (password === "1234")) {
+    console.log("Login Successful!")
+} else {
+    console.log("Admin or Password is incorrect, please try again.")
+}
